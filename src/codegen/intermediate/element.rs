4f38b9631a84809bd0560c4edfb1f40c7402ae94
Siddharth Narayan <siddharth.060605@gmail.com>
2026-08-12 13:03:41 -0400

    just get it started

--- src/codegen/intermediate/element.rs
+++ src/codegen/intermediate/element.rs
@@ -4,10 +4,20 @@ use serde::{Deserialize, Serialize};
 
 use crate::{antlr::ast::EBNFSuffix, codegen::intermediate::alt::{AltIR, TokenAltIR}};
 
-// Should Element really have PartialEq/Eq derived?
 #[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, Hash)]
 pub enum ElementIR {
-    Atom(AtomIR),
+    RuleAtom {
+        id: usize,
+        suffix: Option<EBNFSuffix>
+    },
+    TokenAtom {
+        id: usize,
+        suffix: Option<EBNFSuffix>
+    },
+    TokenSet {
+        id: usize,
+        suffix: Option<EBNFSuffix>
+    },
     Block {
         block: Vec<Arc<AltIR>>,
         suffix: Option<EBNFSuffix>
@@ -15,68 +25,12 @@ pub enum ElementIR {
     // EBNF(EBNF)
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, Hash)]
-pub enum AtomIR {
-    TokenID {
-        id: usize,
-        suffix: Option<EBNFSuffix>
-    },
-
-    RuleID {
-        id: usize,
-        suffix: Option<EBNFSuffix>
-    }
-}
-
-impl AtomIR {
-    pub fn suffix(&self) -> Option<EBNFSuffix> {
-        match self {
-            AtomIR::RuleID { suffix, .. } | AtomIR::TokenID { suffix, .. } => suffix.clone()
-        }
-    }
-
-    pub fn intersects(&self, other: &AtomIR) -> bool {
-        if discriminant(self) != discriminant(other) || self.id() != other.id(){
-            return false
-        }
-
-    }
-}
-
 impl ElementIR {
     pub fn suffix(&self) -> Option<EBNFSuffix> {
         match self {
-            ElementIR::Atom(a) => a.suffix(),
+            ElementIR::RuleAtom { suffix, .. } => *suffix,
+            ElementIR::TokenAtom  { suffix, .. } => *suffix,
             ElementIR::Block { suffix, .. } => *suffix
         }
     }
 }
-
-#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, Hash)]
-pub enum TokenElementIR {
-    Atom {
-        atom: usize,
-        suffix: Option<EBNFSuffix>
-    },
-    Set {
-        inverted: bool,
-        set: BTreeSet<usize>,
-        suffix: Option<EBNFSuffix>
-    },
-    Block {
-        block: Vec<TokenAltIR>,
-        suffix: Option<EBNFSuffix>
-    },
-    // EBNF(EBNF)
-}
-
-impl TokenElementIR {
-    pub fn suffix(&self) -> Option<EBNFSuffix> {
-        match self {
-            TokenElementIR::Atom { suffix, .. } |
-            TokenElementIR::Block { suffix, .. } |
-            TokenElementIR::Set { suffix, .. } => *suffix
-        }
-    }
-}
-
